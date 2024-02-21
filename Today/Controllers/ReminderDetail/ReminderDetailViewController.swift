cf41fec1896fb3166e875caad5891b97723491e8
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2024-02-21 18:38:08 +0300

    style navigation bar

--- Today/Controllers/ReminderDetail/ReminderDetailViewController.swift
+++ Today/Controllers/ReminderDetail/ReminderDetailViewController.swift
@@ -27,15 +27,30 @@ class ReminderDetailViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dataSource = makeDataSource()
+        if #available(iOS 16, *) {
+            navigationItem.style = .navigator
+        }
+//        else {
+//            let appearance = UINavigationBarAppearance()
+//            appearance.configureWithDefaultBackground()
+//            
+//            appearance.backgroundColor = .systemBlue
+//            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+//            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+//
+//            UINavigationBar.appearance().standardAppearance = appearance
+//            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+//        }
+        navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
         
+        dataSource = makeDataSource()
         updateSnapshot()
     }
     
     //
     
     private static func listLayout() -> UICollectionViewCompositionalLayout {
-        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
         listConfiguration.showsSeparators = false
         return UICollectionViewCompositionalLayout.list(using: listConfiguration)
     }
@@ -46,4 +61,9 @@ class ReminderDetailViewController: UICollectionViewController {
         snapshot.appendItems([Row.title, Row.date, Row.time, Row.notes], toSection: 0)
         dataSource.apply(snapshot)
     }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    ReminderDetailViewController(reminder: Reminder.sampleData[0])
 }
